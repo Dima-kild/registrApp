1840e3b1c6f5a754220139b4974cb0e617ecee5e
Dima <di.kild_0510@mail.ru>
2021-08-31 23:01:55 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/registration/UserRegistrationController.java
@@ -0,0 +1,26 @@
+package com.example.demo.registration;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/registration",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.ALL_VALUE)
+@AllArgsConstructor
+public class UserRegistrationController {
+
+    private final RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
+    }
+}
